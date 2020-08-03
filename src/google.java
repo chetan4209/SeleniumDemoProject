670e5869ed1bf45b9ddef39efbc8530bcd6df1aa
chetan4209 <66817539+chetan4209@users.noreply.github.com>
2020-08-03 11:50:12 +0530

    Update google.java

--- src/google.java
+++ src/google.java
@@ -8,11 +8,8 @@ import org.openqa.selenium.remote.server.handler.SendKeys;
 public class google {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.gecko.driver", ".\\Software\\geckodriver.exe"); //launching the browser
-		
 		WebDriver driver=new FirefoxDriver();
-		
 		driver.manage().window().maximize();
-		
 		driver.navigate().to("https://www.google.com");
 		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("java" , Keys.ENTER);
 
